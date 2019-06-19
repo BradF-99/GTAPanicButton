9b432398d7f0f7d8a21bfebc75604f90dd10aa63
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2019-06-20 03:17:16 +1000

    Named all the labels and buttons

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -28,65 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.labelMain = new System.Windows.Forms.Label();
+            this.labelDesc1 = new System.Windows.Forms.Label();
+            this.labelWarn = new System.Windows.Forms.Label();
+            this.labelDesc2 = new System.Windows.Forms.Label();
+            this.btnCredits = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // labelMain
             // 
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 25);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(654, 55);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "GTA Panic Button";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            this.labelMain.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMain.Location = new System.Drawing.Point(12, 25);
+            this.labelMain.Name = "labelMain";
+            this.labelMain.Size = new System.Drawing.Size(654, 55);
+            this.labelMain.TabIndex = 0;
+            this.labelMain.Text = "GTA Panic Button";
+            this.labelMain.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // label2
+            // labelDesc1
             // 
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
-            this.label2.Location = new System.Drawing.Point(12, 93);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(654, 26);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Press Ctrl + Shift + F11 to kill the GTA and Social Club processes.";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            this.labelDesc1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDesc1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.labelDesc1.Location = new System.Drawing.Point(0, 93);
+            this.labelDesc1.Name = "labelDesc1";
+            this.labelDesc1.Size = new System.Drawing.Size(677, 26);
+            this.labelDesc1.TabIndex = 1;
+            this.labelDesc1.Text = "Press Ctrl + Shift + F11 to kill the GTA and Social Club processes.";
+            this.labelDesc1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // label3
+            // labelWarn
             // 
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(0, 170);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(676, 32);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "DO NOT CLOSE THIS WINDOW! Just minimise it.";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelWarn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWarn.Location = new System.Drawing.Point(1, 160);
+            this.labelWarn.Name = "labelWarn";
+            this.labelWarn.Size = new System.Drawing.Size(676, 32);
+            this.labelWarn.TabIndex = 2;
+            this.labelWarn.Text = "DO NOT CLOSE THIS WINDOW! Just minimise it.";
+            this.labelWarn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // label4
+            // labelDesc2
             // 
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
-            this.label4.Location = new System.Drawing.Point(12, 119);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(654, 26);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Press Ctrl + Shift + F12 to suspend GTA for 10 seconds.";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelDesc2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDesc2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.labelDesc2.Location = new System.Drawing.Point(12, 119);
+            this.labelDesc2.Name = "labelDesc2";
+            this.labelDesc2.Size = new System.Drawing.Size(654, 26);
+            this.labelDesc2.TabIndex = 3;
+            this.labelDesc2.Text = "Press Ctrl + Shift + F12 to suspend GTA for 10 seconds.";
+            this.labelDesc2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnCredits
+            // 
+            this.btnCredits.Location = new System.Drawing.Point(303, 202);
+            this.btnCredits.Name = "btnCredits";
+            this.btnCredits.Size = new System.Drawing.Size(75, 35);
+            this.btnCredits.TabIndex = 4;
+            this.btnCredits.Text = "Credits";
+            this.btnCredits.UseVisualStyleBackColor = true;
+            this.btnCredits.Click += new System.EventHandler(this.BtnCredits_Click);
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(678, 227);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(678, 244);
+            this.Controls.Add(this.btnCredits);
+            this.Controls.Add(this.labelDesc2);
+            this.Controls.Add(this.labelWarn);
+            this.Controls.Add(this.labelDesc1);
+            this.Controls.Add(this.labelMain);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "MainWindow";
@@ -97,10 +107,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelMain;
+        private System.Windows.Forms.Label labelDesc1;
+        private System.Windows.Forms.Label labelWarn;
+        private System.Windows.Forms.Label labelDesc2;
+        private System.Windows.Forms.Button btnCredits;
     }
 }
 
