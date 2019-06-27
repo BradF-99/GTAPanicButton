bf0b2f6d0858d4c26551044b373bb16cffd52d93
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2019-06-27 20:03:16 +1000

    Added constructor for ControllerInput class

--- ControllerInput.cs
+++ ControllerInput.cs
@@ -4,9 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using SharpDX.XInput;
+
 namespace GTAPanicButton
 {
     class ControllerInput
     {
+        Controller controller;
+        Gamepad gamepad;
+        public bool connected = false;
+        public float leftTrigger, rightTrigger;
+
+        public ControllerInput()
+        {
+            controller = new Controller(UserIndex.One);
+            connected = controller.IsConnected;
+        }
+
+        // Call this method to update all class values
+        public void Update()
+        {
+            if (!connected)
+                return;
+
+            gamepad = controller.GetState().Gamepad;
+            
+            leftTrigger = gamepad.LeftTrigger;
+            rightTrigger = gamepad.RightTrigger;
+
+            if (leftTrigger > 250 && rightTrigger > 250)
+            {
+                if (gamepad.Buttons == GamepadButtonFlags.LeftShoulder && gamepad.Buttons == GamepadButtonFlags.RightShoulder)
+                {
+                    // quit game
+                }
+                else if (gamepad.Buttons == GamepadButtonFlags.LeftThumb && gamepad.Buttons == GamepadButtonFlags.RightThumb)
+                {
+                    // suspend game
+                }
+            }
+        }
     }
 }
