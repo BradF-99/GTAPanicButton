8e008f52c5e496d131f1b751df1d18270e9ee31a
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2021-07-03 22:09:22 +1000

    Added interaction logic and keybind handling

---
+++ PanicButton/Views/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Interop;
+using PanicButton.Services;
+
+namespace PanicButton.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private HwndSource _source;
+        private WindowInteropHelper helper;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            helper = new WindowInteropHelper(this);
+            _source = HwndSource.FromHwnd(helper.Handle);
+            _source.AddHook(KeybindService.HwndHook);
+            KeybindService.RegisterHotKey(helper);
+        }
+        protected override void OnClosed(EventArgs e)
+        {
+            _source.RemoveHook(KeybindService.HwndHook);
+            _source = null;
+            KeybindService.UnregisterHotKey(helper);
+            base.OnClosed(e);
+        }
+    }
+}
