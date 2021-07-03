3ca902da502ad0c2e10c83d154b96cb8b880d590
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2021-07-03 22:04:22 +1000

    Added Sentry code to app core

---
+++ PanicButton/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using Sentry;
+
+namespace PanicButton
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            using (SentrySdk.Init(o =>
+            {
+                o.Debug = true;
+                o.TracesSampleRate = 1.0;
+            }))
+                DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            SentrySdk.CaptureException(e.Exception);
+            e.Handled = true;
+        }
+    }
+}
