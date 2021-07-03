28c019ed9fb6f64feff4596432d9ce4e9932563e
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2021-07-03 22:05:43 +1000

    Created IListen interface for messaging

---
+++ PanicButton/Interfaces/IListen.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PanicButton.Interfaces
+{
+    public interface IListen { }
+    public interface IListen<in T> : IListen
+    {
+        void Handle(T obj);
+    }
+}
