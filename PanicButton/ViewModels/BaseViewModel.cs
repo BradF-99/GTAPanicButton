78b8d6aaa9c10b378a50c5c7adfbbebd1b0e543c
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2021-07-03 22:07:05 +1000

    Created base view model

---
+++ PanicButton/ViewModels/BaseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using PanicButton.Interfaces;
+using PanicButton.Models;
+
+namespace PanicButton.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged, IListen<SignalMessage>
+    {
+        public virtual void Handle(SignalMessage msg)
+        {
+            throw new NotImplementedException("This should have been over-ridden.");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
